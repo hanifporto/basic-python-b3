5f5d3bfde7657c0e4601ebac1551decd024208e6
hanif <hanifsatrio15@gmail.com>
2021-01-21 20:50:11 +0700

    pertemuan ke 3

---
+++ Break.py
@@ -0,0 +1,8 @@
+i = 1
+while i <= 10:
+  print(i,' x ',i ,' = ',i*i)
+  if i == 5:
+    break
+  i += 1
+
+# berhenti diangka 5
---
+++ for_loop.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana","cherry"]
+
+
+
+
+
+berapa_data = 3
+for j in range(berapa_data):
+    fruits.append(input("input data:  "))
+
+print (fruits)
+
